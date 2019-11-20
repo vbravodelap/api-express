2947c718eadcc8fea1ade7451482a743d11b84f4
Victor Bravo <vbravodelap@gmail.com>
2019-11-19 22:34:57 -0600

    cors added

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ var comment_routes = require('./routes/comment');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 app.use('/api', user_routes);
 app.use('/api', topic_routes);
 app.use('/api', comment_routes);
